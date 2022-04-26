ef3dad3b0a865a71a308dcf9903a315b5b1ee35c
marcos-asdes <marco.engc@poli.ufrj.br>
2022-04-26 05:10:01 -0300

    added tweets get and tweets post

--- index.js
+++ index.js
@@ -14,7 +14,24 @@ app.post("/sign-up", (req, res) => {
     const body = req.body; /* json -> obj (parser) */
     users.push(body); /* do in the future a data treatment in this body to not receive errors */
     res.send("Ok"); /* cod 200 */
-})
+});
+
+app.post("/tweets", (req, res) => {
+    const body = req.body; /* json -> obj (parser) */
+    const {username, tweet} = body;
+    const {avatar} = users.find(user => user.username === username);
+    tweets.push({
+        username, 
+        avatar,
+        tweet
+    });
+    res.send("Ok"); /* cod 200 */
+});
+
+app.get("/tweets", (red, res) => {
+    if(tweets.length <= 10) res.send(tweets); /* fix ~ all tweets in decrescent order*/
+    else res.send(tweets) /* fix ~ last 10 tweets in decrescent order */
+});
 
 app.listen(5000, () => {
     console.log(chalk.bold.green("active server on port 5000"));
