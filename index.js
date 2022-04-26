3a2768420b9e663113a2e06984eab1249251f2eb
marcos-asdes <marco.engc@poli.ufrj.br>
2022-04-26 04:59:10 -0300

    added sign up mode

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import chalk from "chalk";
-import express from "express";
+import express, {json} from "express";
+import cors from "cors";
 
 const app = express();
+app.use(json()); /* parser */
+app.use(cors()); /* middleware (headers) */
+
+const tweets = []; /* username, avatar, tweet */
+const users = []; /* username, avatar */
+/* the tweet's avatar information comes from the user */
+
+app.post("/sign-up", (req, res) => {
+    const body = req.body; /* json -> obj (parser) */
+    users.push(body); /* do in the future a data treatment in this body to not receive errors */
+    res.send("Ok"); /* cod 200 */
+})
 
 app.listen(5000, () => {
     console.log(chalk.bold.green("active server on port 5000"));
